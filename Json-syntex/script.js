280a0a4d3601d9f941d319f8efa7681abf9a1d52
mansoorkhan7787 <kmr967474@gmail.com>
2025-04-14 03:14:38 -0700

    first commit

---
+++ Json-syntex/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $.ajax({
+        url : "./product.json",
+        type : "GET",
+        success : function(response) {
+            // console.log(response)
+            let x = " ";
+            $.each(response,function(keys, object) {
+                // console.log(object)
+                x+= `
+                <tr> 
+                    <td>${object.id} </td>
+                    <td>${object.name}</td>
+                    <td>${object.price}</td>
+                    <td>${object.sell}</td>
+                    <td>${object.description}</td>
+                    <td> <img src="${object.image}" width="100"></td>
+                </tr> `
+
+            })
+            document.getElementById("data").innerHTML=x;
+        }
+    })
+})
